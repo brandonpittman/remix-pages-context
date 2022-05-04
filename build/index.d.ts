994819328999815bb94f1043fe607153f70906c4
Brandon Pittman <brandon@blp.is>
2022-05-04 23:35:36 +0900

    remove /build from git repo

--- build/index.d.ts
+++
@@ -1,4 +0,0 @@
-declare let pagesContext: Record<string, any>;
-export declare let setPagesContext: (context: typeof pagesContext) => Record<string, any>;
-export declare let getPagesContext: () => Record<string, any>;
-export {};
