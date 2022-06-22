f9fb147c50cff15ca9334264d29efe020a0d85bd
Brandon Pittman <brandon@blp.is>
2022-06-22 10:17:31 +0900

    Use tiny-invariant

--- src/session.ts
+++ src/session.ts
@@ -3,23 +3,23 @@ import {
   createCloudflareKVSessionStorage,
   CookieOptions,
 } from "@remix-run/cloudflare";
+import invariant from "tiny-invariant";
 
 import { getPagesContext } from "./index";
 
-const SECONDS_IN_DAY = 86400;
-const YEAR = SECONDS_IN_DAY * 365;
+let SECONDS_IN_DAY = 86400;
+let YEAR = SECONDS_IN_DAY * 365;
 let expires = new Date(Date.now() + YEAR * 1000);
 let maxAge = YEAR;
 
-export const getSessionStorage = (
+export let getSessionStorage = (
   env = getPagesContext(),
   options?: CookieOptions
 ) => {
-  if (!env.SESSION_SECRET) throw new Error("SESSION_SECRET is not defined");
+  invariant(!env.SESSION_SECRET, "SESSION_SECRET is not defined");
+  invariant(!env.KV, "KV namespace, KV, is not defined");
 
-  if (!env.KV) throw new Error("KV namespace, KV, is not defined");
-
-  const cookie = createCookie("__session", {
+  let cookie = createCookie("__session", {
     expires,
     maxAge,
     secrets: [env.SESSION_SECRET],
